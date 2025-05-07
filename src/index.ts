c08b5e2fe84d020015206739db22ce31dc01ea9a
lamontanarog <giulianalamontanaro@gmail.com>
2025-05-07 12:16:35 -0300

    bug:CORS ERROR

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:'*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use(helmet());
 
 export const prisma = new PrismaClient();
